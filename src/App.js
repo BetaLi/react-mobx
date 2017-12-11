82f76674d548a1101f3e8bfea88e218a2cb418d1
Lcoder <linelijun@163.com>
2017-12-11 19:26:05 +0800

    upadte observable\computed\autorun

--- src/App.js
+++ src/App.js
@@ -7,25 +7,160 @@ import {observable, computed, action, autorun} from 'mobx';
 class App extends Component {
 
     render(){
-        const todoStore = observable({
-            todos:[],
-            get completedCount(){
-                return this.todos.filter(todo=>todo.completed).length;
+        const age = observable(10);
+        const dispose = autorun(()=>{
+            if(age.get()<0){
+                throw new Error("age error")
             }
+            console.log("Age is %d ",age.get());
         });
-        autorun(()=>{
-            console.log('Completed %d of %d items',
-                todoStore.completedCount,
-                todoStore.todos.length);
+        age.set(15);
+
+        dispose.onError(e =>{
+           alert("Please set a correct number.");
         });
-        todoStore.todos.push({title:'Go for tea.',completed:false});
-        todoStore.todos[0].completed = true;
+        age.set(-10);
         return (
-            <div>
-                <h2>See console result by press F12</h2>
-            </div>
-        )
+            <div>See result at console.</div>
+        );
     }
+
+
+
+
+    // render(){
+    //     const number = observable([1,2,3]);
+    //     const sum = computed(()=>number.reduce((a,b)=>a+b,0));
+    //     const disposer = autorun(()=>console.log(sum.get()));
+    //     number.push(4);
+    //     disposer();
+    //     number.push(5);
+    //     return (
+    //         <div>See result at console.</div>
+    //     )
+    // }
+
+
+
+
+
+    // render(){
+    //     const x = observable(1);
+    //     const y = observable(3);
+    //
+    //     const result = computed(()=>{
+    //         if(x.get()===0){
+    //             throw new Error('discovery a error');
+    //         }else {
+    //             return y.get()/x.get();
+    //         }
+    //     });
+    //     y.set(15);
+    //     x.set(12);
+    //     //x.set(0);
+    //     return (
+    //         <div>{result.get()}</div>
+    //     );
+    // }
+
+
+    
+    // render(){
+    //     const name = observable("Jone");
+    //
+    //     const upCaseName = computed(()=>{
+    //         name.get().toUpperCase();
+    //     });
+    //     upCaseName.observe(change=>console.log(change.newValue));
+    //     name.set("Dave");
+    //     return (
+    //         <div></div>
+    //     )
+    // }
+    
+
+    // render(){
+    //     class Foo{
+    //         @observable length = 2;
+    //         @computed get Square(){
+    //             return this.length*this.length;
+    //         }
+    //         // set Squar(value){
+    //         //    return this.length = Math.sqrt(value);
+    //         // }
+    //     }
+    //     const foo = new Foo();
+    //     return (
+    //         <div>
+    //             {foo.Square}
+    //         </div>
+    //     )
+    // }
+
+
+
+
+
+    // render(){
+    //     class OrderLine{
+    //         @observable price = 0;
+    //         @observable count = 1;
+    //
+    //         @computed get total(){
+    //             return this.price*this.count;
+    //         }
+    //     }
+    //     const orderLine = new OrderLine();
+    //     return (
+    //         <div>
+    //             {orderLine.total}
+    //         </div>
+    //     )
+    // }
+
+
+
+
+    // render(){
+    //     const map = observable.map({"key":"value"});
+    //     map.set("key","Flash");
+    //
+    //     const array = observable([1,2,3]);
+    //     array[1] = 10;
+    //
+    //     const temperature = observable(20);
+    //     temperature.set(25);
+    //
+    //     autorun(()=>{
+    //         console.log(map.get("key")+"   "+array+"    "+temperature);
+    //     });
+    //     return (
+    //         <div>
+    //             See result by press F12.
+    //         </div>
+    //     )
+    // }
+
+    // render(){
+    //     const todoStore = observable({
+    //         todos:[],
+    //         get completedCount(){
+    //             return this.todos.filter(todo=>todo.completed).length;
+    //         }
+    //     });
+    //     autorun(()=>{
+    //         console.log('Completed %d of %d items',
+    //             todoStore.completedCount,
+    //             todoStore.todos.length);
+    //     });
+    //     todoStore.todos.push({title:'Go for tea.',completed:false});
+    //     todoStore.todos[0].completed = true;
+    //     return (
+    //         <div>
+    //             <h2>See console result by press F12 in Chrome</h2>
+    //         </div>
+    //     )
+    // }
 
 
 
